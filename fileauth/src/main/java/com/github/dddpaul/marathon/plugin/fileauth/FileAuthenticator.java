2c001a43275e04545ad3282b615b7c4eee50c63a
Pavel Derendyaev <pderendyaev@alfabank.ru>
2019-01-01 20:03:42 +0300

    Use map instead list

--- fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/FileAuthenticator.java
+++ fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/FileAuthenticator.java
@@ -3,8 +3,8 @@ package com.github.dddpaul.marathon.plugin.fileauth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dddpaul.marathon.plugin.auth.BasicAuthenticator;
 import com.github.dddpaul.marathon.plugin.auth.JavaIdentity;
-import com.github.dddpaul.marathon.plugin.fileauth.conf.AuthenticatorConfiguration;
 import com.github.dddpaul.marathon.plugin.fileauth.conf.AuthenticatorConfigurationHolder;
+import com.github.dddpaul.marathon.plugin.fileauth.conf.AuthenticatorConfigurationHolder.AuthenticatorConfiguration;
 import com.github.dddpaul.marathon.plugin.fileauth.entities.User;
 import mesosphere.marathon.plugin.auth.Identity;
 import mesosphere.marathon.plugin.plugin.PluginConfiguration;
@@ -17,19 +17,21 @@ public class FileAuthenticator extends BasicAuthenticator implements PluginConfi
     private AuthenticatorConfiguration configuration;
 
     @Override
-    public void initialize(scala.collection.immutable.Map<String, Object> marathonInfo, JsObject configuration) {
+    public void initialize(scala.collection.immutable.Map<String, Object> marathonInfo, JsObject json) {
+        logger.info("Authenticator initialize has been called with: " + json);
         ObjectMapper mapper = new ObjectMapper();
         try {
-            this.configuration = mapper.readValue(configuration.toString(), AuthenticatorConfigurationHolder.class).getConfiguration();
+            configuration = mapper.readValue(json.toString(), AuthenticatorConfigurationHolder.class).getConfiguration();
         } catch (IOException e) {
             logger.error("Plugin configuration parsing has been failed", e);
-            this.configuration = new AuthenticatorConfiguration();
+            configuration = new AuthenticatorConfiguration();
         }
     }
 
     @Override
     protected Identity doAuth(String username, String password) {
-        if (configuration.getUsers().contains(new User(username, password))) {
+        User user = configuration.getUsers().get(username);
+        if (user != null && password.equals(user.getPassword())) {
             return new JavaIdentity(username);
         } else {
             return null;
--- fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/conf/AuthenticatorConfigurationHolder.java
+++ fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/conf/AuthenticatorConfigurationHolder.java
@@ -2,6 +2,9 @@ package com.github.dddpaul.marathon.plugin.fileauth.conf;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.dddpaul.marathon.plugin.fileauth.entities.User;
+
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AuthenticatorConfigurationHolder {
@@ -11,5 +14,16 @@ public class AuthenticatorConfigurationHolder {
 
     public AuthenticatorConfiguration getConfiguration() {
         return configuration;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class AuthenticatorConfiguration {
+
+        @JsonProperty("users")
+        private Map<String, User> users;
+
+        public Map<String, User> getUsers() {
+            return users;
+        }
     }
 }
