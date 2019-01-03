c1595846710a60666c6ea0d99d1de6edf9d481c3
Pavel Derendyaev <pderendyaev@alfabank.ru>
2019-01-03 17:57:56 +0300

    Add lombok

--- fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/entities/Permission.java
+++ fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/entities/Permission.java
@@ -1,16 +1,9 @@
 package com.github.dddpaul.marathon.plugin.fileauth.entities;
 
+import lombok.Data;
+
+@Data
 public class Permission {
-
     private String role;
-
     private String path;
-
-    public String getRole() {
-        return role;
-    }
-
-    public String getPath() {
-        return path;
-    }
 }
--- fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/entities/Role.java
+++ fileauth/src/main/java/com/github/dddpaul/marathon/plugin/fileauth/entities/Role.java
@@ -1,13 +1,11 @@
 package com.github.dddpaul.marathon.plugin.fileauth.entities;
 
 import com.github.dddpaul.marathon.plugin.auth.Action;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class Role {
     private List<Action> actions;
-
-    public List<Action> getActions() {
-        return actions;
-    }
 }
