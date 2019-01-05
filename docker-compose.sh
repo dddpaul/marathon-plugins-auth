86605df3e81959cc92d3bf452a565ad18cf525a2
Pavel Derendyaev <pderendyaev@alfabank.ru>
2019-01-05 17:53:33 +0300

    Update scripts

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
-yq . src/main/resources/plugin-conf.yml > src/main/resources/plugin-conf.json
-./gradlew clean shadowJar
-docker-compose -f src/test/resources/docker-compose.yml up
+
+usage="Usage: docker-compose.sh up|down|cluster-up|cluster-down"
+
+function prepare() {
+    yq . src/main/resources/plugin-conf.yml > src/main/resources/plugin-conf.json
+    ./gradlew clean shadowJar
+}
+
+case "$1" in
+    up)
+        prepare
+        docker-compose -f src/test/resources/docker-compose.yml up
+        ;;
+    down)
+        docker-compose -f src/test/resources/docker-compose.yml down
+        ;;
+    cluster-up)
+        prepare
+        docker-compose -f src/test/resources/docker-compose-cluster.yml up
+        ;;
+    cluster-down)
+        docker-compose -f src/test/resources/docker-compose-cluster.yml down
+        ;;
+    *)
+        echo ${usage}
+esac
